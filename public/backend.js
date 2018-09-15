aafcaef09ca92d257e87b48103b1153f00733b29
nquazi <noushinquazi@yahoo.com>
2018-09-14 22:14:48 -0500

    added logging in

--- public/backend.js
+++ public/backend.js
@@ -8,11 +8,10 @@ document.addEventListener('DOMContentLoaded', function() {
   // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
   //
   // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-  window.db = firebase.database();
+  window.db = firebase.database()
   window.eventList = db.ref('events')
-  createEvent("test2", "RMC", new Date().getTime(), new Date().getTime() + 50000)
-  getEvents()
-});
+  window.googleProvider = new firebase.auth.GoogleAuthProvider()
+})
 
 function createEvent(title, loc, startTime, endTime) {
   let newEvent = eventList.push()
@@ -28,9 +27,27 @@ function getEvents() {
   eventList.once('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
-      var childData = childSnapshot.val();
+      var childData = childSnapshot.val()
       console.log(childKey, childData)
     });
   });
 }
 
+function logIn() {
+  firebase.auth().signInWithPopup(googleProvider).then(function(result) {
+    var token = result.credential.accessToken
+    var user = result.user
+
+    window.user = user
+    db.ref(`users/${user.uid}`).update({
+        name: user.displayName,
+        email: user.email.toLowerCase(),
+    })
+
+   }).catch(function(error) {
+    var errorCode = error.code
+    var errorMessage = error.message
+    var email = error.email
+    var credential = error.credential
+  })
+}
